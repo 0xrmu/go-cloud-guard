d9de4efd883f765d2855bf3506a183f9d38cb916
0xrmu <xfahad201@gmail.com>
2026-09-13 09:17:00 +0300

    list bucket function and edited client.go adding a client call to it

--- main.go
+++ main.go
@@ -1,18 +1,27 @@
-package gocloudguard
+package main
 
 import (
 	"context"
 	"fmt"
 
-	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/0xrmu/go-cloud-guard/pkg/aws"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
 func main() {
 	ctx := context.Background()
-	cfg, err := config.LoadDefaultConfig(ctx)
+	s3client, err := aws.NewS3Client(ctx)
 	if err != nil {
 		panic(err)
 	}
-	
-	fmt.Println(cfg)
+
+	result, err := s3client.ListBuckets(ctx, &s3.ListBucketsInput{})
+	if err != nil {
+		panic(err)
+	}
+
+	for _, buckets := range result.Buckets {
+		fmt.Println(*buckets.Name)
+	}
+
 }
