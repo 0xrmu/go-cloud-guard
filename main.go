850c6c8730e90e6b9751bad331169bb4a5466c15
0xrmu <xfahad201@gmail.com>
2026-09-20 12:32:32 +0300

    Detect security groups exposed to 0.0.0.0/0

--- main.go
+++ main.go
@@ -5,12 +5,18 @@ import (
 	"fmt"
 
 	"github.com/0xrmu/go-cloud-guard/pkg/aws"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
 func main() {
 	ctx := context.Background()
 	s3client, err := aws.NewS3Client(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	ec2client, err := aws.NewEC2Client(ctx)
 	if err != nil {
 		panic(err)
 	}
@@ -24,4 +30,18 @@ func main() {
 		fmt.Println(*buckets.Name)
 	}
 
+	result1, err1 := ec2client.DescribeSecurityGroups(ctx, &ec2.DescribeSecurityGroupsInput{})
+	if err1 != nil {
+		panic(err1)
+	}
+
+	for _, securityGroup := range result1.SecurityGroups {
+		for _, permission := range securityGroup.IpPermissions {
+			for _, ipRange := range permission.IpRanges {
+				if ipRange.CidrIp != nil && *ipRange.CidrIp == "0.0.0.0/0" {
+					fmt.Println("EXPOSED:", *securityGroup.GroupName)
+				}
+			}
+		}
+	}
 }
--- pkg/aws/client.go
+++ pkg/aws/client.go
@@ -1,18 +1,30 @@
 package aws
 
-
 import (
 	"context"
+
 	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
 func NewS3Client(ctx context.Context) (*s3.Client, error) {
 	cfg, err := config.LoadDefaultConfig(ctx)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
+	cfg.Region = "eu-north-1"
 
 	s3client := s3.NewFromConfig(cfg)
 	return s3client, nil
+}
+
+func NewEC2Client(ctx context.Context) (*ec2.Client, error) {
+	cfg, err := config.LoadDefaultConfig(ctx)
+	if err != nil {
+		return nil, err
+	}
+	cfg.Region = "eu-north-1"
+	ec2client := ec2.NewFromConfig(cfg)
+	return ec2client, nil
 }
